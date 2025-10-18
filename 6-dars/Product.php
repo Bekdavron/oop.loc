021ad9675a54b154847b73d2668558bcc4f67238
Bekdavron <bekdavron401@gmail.com>
2025-10-18 17:21:02 +0500

    php 8 Refactor Product constructor to use union types for price and set default values

--- 6-dars/Product.php
+++ 6-dars/Product.php
@@ -1,14 +1,18 @@
 <?php
 class Product
 {
-  public string $name;
-  public float $price;
+  //   public string $name;
+  //   public float $price;
 
-  public function __construct(string $name, float $price)
-  {
-    $this->name = $name;
-    $this->price = $price;
-  }
+  //   public function __construct(string $name, float $price)
+  //   {
+  //     $this->name = $name;
+  //     $this->price = $price;
+  //   }
+  public function __construct(public string $name = "Some Product", public int | float $price = 0) {}
+
+
+
   public function getRealPrice($currency = '$'): string
   {
     return "{$currency}" . $this->price / 100;
